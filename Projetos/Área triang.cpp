69c148734d63b4585881363c9f15efa2aaaf9aa3
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-10-04 21:25:41 -0300

    Área do Triângulo

---
+++ Projetos/Área triang.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL,"");
+	float alt,base,area;
+	printf("Informe a altura: ");
+	scanf("%f",&alt);
+	printf("Informe a base: ");
+	scanf("%f",&base);
+	area=base*alt/2;
+	printf("A área do triângulo é igual a: %.1f",area);
+	return 0;
+}
