458ac5ffe66591b29cbf873af1c479fd8f715a17
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-10-06 21:37:43 -0300

    Tabuada simples

---
+++ Projetos/Tabuada.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	int cont=0,num;
+	{	
+		setlocale(LC_ALL,"");
+		printf("——————Tabuada——————\n\n");
+		printf("Informe um número: ");
+		scanf("%d",&num);
+		for(cont=0;cont<=10;cont++)
+		printf("\n%d x %d = %d\n",num, cont, num*cont);
+	}
+	return 0;
+}
