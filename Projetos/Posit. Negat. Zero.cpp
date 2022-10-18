da6e0d6b8c874428e09bde2d624a1139d80ba4ee
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-10-18 20:19:03 -0300

    Valor positivo / Negativo / Zero

---
+++ Projetos/Posit. Negat. Zero.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+    main( )
+{
+	  setlocale(LC_ALL,"");	
+	  int x;
+	  printf("Informe um número: ");
+	  scanf("%i",&x);
+	  if (x>0)
+	{
+		printf("\nO valor é: Positivo\n");
+	}
+	  else if (x<0)
+	{
+		printf("\nO valor é: Negativo\n");
+    }
+      else
+    {
+    	printf("\nO valor é: 0\n");	
+	}  
+}     
+
