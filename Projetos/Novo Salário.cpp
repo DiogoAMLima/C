31a03906b84153a77bb760c4941a02b4691bd56c
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-10-22 11:10:36 -0300

    Aumento de salário

---
+++ Projetos/Novo Salário.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	float sal,novo_sal;
+	setlocale(LC_ALL,"");
+	printf("Informe o salário do funcionário: ");
+	scanf("%f",&sal);
+	novo_sal=(sal*25/100)+sal;
+	printf("\nO novo salário com aumento é: %.f",novo_sal);
+	return 0;
+}
