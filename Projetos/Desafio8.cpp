d7b6b8ad47581e079f892ee0015daab6d8942e19
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-11-14 10:15:23 -0300

    Desafio 8

---
+++ Projetos/Desafio8.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+int fat(int N)
+{
+	if (N < 2)
+	return 1;
+	else
+    return N*fat(N-1);
+}
+
+int main()
+{
+	int N;
+	setlocale(LC_ALL,"");
+	printf("Digite um num: ");
+	scanf("%d",&N);
+	printf("\nO fatorial de %d é: %d ",N, fat(N));
+}
