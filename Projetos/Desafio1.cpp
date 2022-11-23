a1ab7ee53be816d2c2c4de833e648967edf3ac3c
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-11-23 20:58:42 -0300

    Desafio 1

--- Projetos/Desafio1.cpp
+++ Projetos/Desafio1.cpp
@@ -3,21 +3,21 @@
 #include <locale.h>
 
 int main(){
-	int n1,n2,n3,n4,SQ;
-	setlocale(LC_ALL,"");
-	printf("Digite um num: ");
-	scanf("%d",&n1);
-	printf("\nDigite um num: ");
-	scanf("%d",&n2);
-	printf("\nDigite um num: ");
-	scanf("%d",&n3);
-	printf("\nDigite um num: ");
-	scanf("%d",&n4);
-	SQ=(n1*n1)+(n2*n2)+(n3*n3)+(n4*n4);
-	if((n3*n3)>=1000){
-		printf("\n%.5d \n",(n3*n3));
-	}
-	printf("\nNúmeros digitados ao quadrado, depois somados, resultam em: %d",SQ);
-	return 0;
-	system ("pause");
+    int n1,n2,n3,n4,SQ;
+    setlocale(LC_ALL,"");
+    printf("Digite um num: ");
+    scanf("%d",&n1);
+    printf("\nDigite um num: ");
+    scanf("%d",&n2);
+    printf("\nDigite um num: ");
+    scanf("%d",&n3);
+    printf("\nDigite um num: ");
+    scanf("%d",&n4);
+    SQ=(n1*n1)+(n2*n2)+(n3*n3)+(n4*n4);
+    if((n3*n3)>=1000){
+        printf("\n%.5d \n",(n3*n3));
+    }
+    printf("\nNÃºmeros digitados ao quadrado, depois somados, resultam em: %d",SQ);
+    return 0;
+    system ("pause");
 }
