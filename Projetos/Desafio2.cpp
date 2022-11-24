9d278a3a6d295cdf0614c5cd5f5f17806b041ffe
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-11-23 21:00:20 -0300

    Desafio 2

--- Projetos/Desafio2.cpp
+++ Projetos/Desafio2.cpp
@@ -3,29 +3,29 @@
 #include <locale.h>
 
 int main(){
-	float n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
-	float M;
-	setlocale(LC_ALL,"");
-	printf("Digite o primeiro número: ");
-	scanf("%f",&n1);
-	printf("Digite o segundo número: ");
-	scanf("%f",&n2);
-	printf("Digite o terceiro número: ");
-	scanf("%f",&n3);
-	printf("Digite o quarto número: ");
-	scanf("%f",&n4);
-	printf("Digite o quinto número: ");
-	scanf("%f",&n5);
-	printf("Digite o sexto número: ");
-	scanf("%f",&n6);
-	printf("Digite o sétimo número: ");
-	scanf("%f",&n7);
-	printf("Digite o oitavo número: ");
-	scanf("%f",&n8);
-	printf("Digite o nono número: ");
-	scanf("%f",&n9);
-	printf("Digite o décimo número: ");
-	scanf("%f",&n10);
-	M=(n1+n2+n3+n4+n5+n6+n7+n8+n9+n10)/10;
-	printf("A média dos números digitados é: %.2f", M);
+    float n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
+    float M;
+    setlocale(LC_ALL,"");
+    printf("Digite o primeiro nÃºmero: ");
+    scanf("%f",&n1);
+    printf("Digite o segundo nÃºmero: ");
+    scanf("%f",&n2);
+    printf("Digite o terceiro nÃºmero: ");
+    scanf("%f",&n3);
+    printf("Digite o quarto nÃºmero: ");
+    scanf("%f",&n4);
+    printf("Digite o quinto nÃºmero: ");
+    scanf("%f",&n5);
+    printf("Digite o sexto nÃºmero: ");
+    scanf("%f",&n6);
+    printf("Digite o sÃ©timo nÃºmero: ");
+    scanf("%f",&n7);
+    printf("Digite o oitavo nÃºmero: ");
+    scanf("%f",&n8);
+    printf("Digite o nono nÃºmero: ");
+    scanf("%f",&n9);
+    printf("Digite o dÃ©cimo nÃºmero: ");
+    scanf("%f",&n10);
+    M=(n1+n2+n3+n4+n5+n6+n7+n8+n9+n10)/10;
+    printf("A mÃ©dia dos nÃºmeros digitados Ã©: %.2f", M);
 }
