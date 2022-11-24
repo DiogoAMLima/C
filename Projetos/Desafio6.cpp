475710b2deefbce622e78eb10193ebdb23435589
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-11-23 21:05:25 -0300

    Desafio 6

--- Projetos/Desafio6.cpp
+++ Projetos/Desafio6.cpp
@@ -5,21 +5,21 @@
 
 float volcil(float x,float y)
 {
-	float R,H,PI,volumecilindro;
+    float R,H,PI,volumecilindro;
     PI=3.141592;
-	R=x;
-	H=y;
-	volumecilindro=PI*R*R*H;
-	return volumecilindro;
+    R=x;
+    H=y;
+    volumecilindro=PI*R*R*H;
+    return volumecilindro;
 }
 int main()
 {
 float R,H,volumecilindro;
 
-	setlocale(LC_ALL,"");
-	printf("Digite o valor do raio: ");
-	scanf("%f",&R);
-	printf("\nDigite a altura: ");
-	scanf("%f",&H);
-	printf("\nO volume do cilindro é: %.2f",volcil(R,H));
+    setlocale(LC_ALL,"");
+    printf("Digite o valor do raio: ");
+    scanf("%f",&R);
+    printf("\nDigite a altura: ");
+    scanf("%f",&H);
+    printf("\nO volume do cilindro Ã©: %.2f",volcil(R,H));
 }
