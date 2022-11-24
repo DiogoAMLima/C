c3788028331b9cc0a162220f275f3f24b7dc3b5d
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-11-23 21:06:06 -0300

    Desafio 7

--- Projetos/Desafio7.cpp
+++ Projetos/Desafio7.cpp
@@ -2,16 +2,16 @@
 #include <locale.h>
 
 int soma(int n){
-	if(n<0)
-	return 0;
-	else
-	return n+soma(n-1);
+    if(n<0)
+    return 0;
+    else
+    return n+soma(n-1);
 }
 main()
 {
-	int num;
-	setlocale(LC_ALL,"");
-	printf("Digite um número maior que 0: ");
-	scanf("%d",&num);
-	printf("\nSoma = %d",soma(num));
+    int num;
+    setlocale(LC_ALL,"");
+    printf("Digite um nÃºmero maior que 0: ");
+    scanf("%d",&num);
+    printf("\nSoma = %d",soma(num));
 }
