bbdd82585aeb7b517516586ca6c5d08fef4339a5
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-11-29 20:22:23 -0300

    Função 6

---
+++ Projetos/Função 6.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+float hip(float x,float y)
+{
+	float a,b,hipotenusa;
+	a=x;
+	b=y;
+	hipotenusa=(a*a)+(b*b);
+	return hipotenusa;
+}
+int main()
+{
+float a,b,hipotenusa;
+	setlocale(LC_ALL,"");
+	printf("Digite o valor de a: ");
+	scanf("%f",&a);
+	printf("\nDigite o valor de b: ");
+	scanf("%f",&b);
+	printf("\nO valor da hipotenusa é: %.2f",hip(a,b));
+}
