1b7ca4c79b17cf37a54d5bc578e4f370a99e86f1
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-01 00:20:19 -0300

    Função 8

---
+++ Projetos/Função 8.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+float fah(float x)
+{
+float F,C;
+ C=x;
+ F=(C*9/5)+32;	
+ return F;	
+}
+int main()
+{
+ float C;
+  setlocale(LC_ALL,"");
+  printf("Digite o valor em celsius: ");
+  scanf("%f",&C);
+  printf("\nO valor em fahrenheit é: %.2f",fah(C));
+	
+}
