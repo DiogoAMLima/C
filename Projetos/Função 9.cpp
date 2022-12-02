75c4edf632d0e3db1e9bd692cf096244dc7c7a79
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-02 00:06:43 -0300

    Função 9

--- Projetos/Função 9.cpp
+++ Projetos/Função 9.cpp
@@ -13,15 +13,15 @@ int segds(int x,int y,int z)
 }
 int main()
 {
-	int H,M,S;
-	setlocale(LC_ALL,"");
-	printf("Digite a quantidade de horas: ");
-	scanf("%d",&H);
-	printf("Digite a quantidade de minutos: ");
-	scanf("%d",&M);
-	printf("Digite a quantidade de segundos: ");
-	scanf("%d",&S);
-	H=H*3600;
+    int H,M,S;
+    setlocale(LC_ALL,"");
+    printf("Digite a quantidade de horas: ");
+    scanf("%d",&H);
+    printf("Digite a quantidade de minutos: ");
+    scanf("%d",&M);
+    printf("Digite a quantidade de segundos: ");
+    scanf("%d",&S);
+    H=H*3600;
     M=M*60;
-	printf("\nA quantidade de segundos em horas, %d , a quantidade de segundos em minutos %d e a quantidade de segundos %d",H,M,S,segds(H,M,S));
+    printf("\nA quantidade de segundos em horas, %d , a quantidade de segundos em minutos %d e a quantidade de segundos %d",H,M,S,segds(H,M,S));
 }
