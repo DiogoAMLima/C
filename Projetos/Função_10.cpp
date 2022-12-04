e06127cfaa77942236572d6ea7c745b61148892b
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-04 17:28:04 -0300

    Função 10

--- Projetos/Função_10.cpp
+++ Projetos/Função_10.cpp
@@ -4,23 +4,22 @@
 
 float pond(float x,float y,float z)
 {
-	float D,E,F,pond1;
-	D=x;
-	E=y;
-	F=z;
-	pond1=(D*5+E*3+F*2)/10;
-	return pond1;
+    float D,E,F,pond1;
+    D=x;
+    E=y;
+    F=z;
+    pond1=(D*5+E*3+F*2)/10;
+    return pond1;
 }
 int main()
 {	
-	float D,E,F;
-	setlocale(LC_ALL,"");
-	printf("Digite a primeira nota: ");
-	scanf("%f",&D);
-	printf("\nDigite a segunda nota: ");
-	scanf("%f",&E);
-	printf("\nDigite a terceira nota: ");
-	scanf("%f",&F);	
-	printf("\nA média ponderada é: %.2f",pond(D,E,F));
+    float D,E,F;
+    setlocale(LC_ALL,"");
+    printf("Digite a primeira nota: ");
+    scanf("%f",&D);
+    printf("\nDigite a segunda nota: ");
+    scanf("%f",&E);
+    printf("\nDigite a terceira nota: ");
+    scanf("%f",&F);	
+    printf("\nA mÃ©dia ponderada Ã©: %.2f",pond(D,E,F));
 }
-
