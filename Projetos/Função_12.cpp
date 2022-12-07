5cb2a2407a016b93ae7b72f4b87b8ffe255aa732
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 01:09:05 -0300

    Função 12

--- Projetos/Função_12.cpp
+++ Projetos/Função_12.cpp
@@ -5,21 +5,21 @@
 
 int quadprft(int x)
 {
-	int num,quadradoprft;
-	num=x;
-	quadradoprft=num*num;
-	return quadradoprft;
+    int num,quadradoprft;
+    num=x;
+    quadradoprft=num*num;
+    return quadradoprft;
 }
 int main()
 {
-	int num;
-	setlocale(LC_ALL,"");
-	printf("Digite um n˙mero inteiro n„o negativo: ");
-	scanf("%d",&num);
-	if((num>0) && (num%2==0))
-	printf("\nN˙mero inteiro e n„o negativo registrado...\n");
-	else{
-	printf("\nN„o È um n˙mero inteiro ou È um n˙mero negativo\n");
+    int num;
+    setlocale(LC_ALL,"");
+    printf("Digite um n√∫mero inteiro n√£o negativo: ");
+    scanf("%d",&num);
+    if((num>0) && (num%2==0))
+    printf("\nN√∫mero inteiro e n√£o negativo registrado...\n");
+    else{
+    printf("\nN√£o √© um n√∫mero inteiro ou √© um n√∫mero negativo\n");
 }
-	printf("\nO quadrado perfeito do n˙mero È: %d",quadprft(num));
+    printf("\nO quadrado perfeito do n√∫mero √©: %d",quadprft(num));
 }
