e9228760f66624bd7f5ffba477d0c080fc3dd76c
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 20:59:19 -0300

    Área Círculo

--- Math/Área Círculo.cpp
+++ Math/Área Círculo.cpp
@@ -5,13 +5,13 @@ const float pi=3.141592;
 int main(){
   setlocale(LC_ALL,"");
   float raio, area, perimetro;
-  printf("A constante PI est· sendo considerada como: 3.141592");
+  printf("A constante PI est√° sendo considerada como: 3.141592");
   printf("\n\nDigite o raio: ");
   scanf("%f",&raio);
   area = pi * (raio*raio);
   perimetro=2.0 * pi * raio;
-  printf("\nO Raio È: %.1f",raio);
-  printf("\n\nA ·rea: %.1f\n",area);
-  printf("\nO PerÌmetro È: %.1f\n",perimetro);
+  printf("\nO Raio √©: %.1f",raio);
+  printf("\n\nA √°rea: %.1f\n",area);
+  printf("\nO Per√≠metro √©: %.1f\n",perimetro);
   return 0;
 }
