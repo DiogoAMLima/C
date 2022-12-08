bfcd19bb3bd7b0d88f76a57d928587016f4f2430
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 21:16:19 -0300

    Operação de 2 núm

--- Math/Operação de 2 núm.cpp
+++ Math/Operação de 2 núm.cpp
@@ -2,32 +2,32 @@
 #include <locale.h>
 
 int main(){
-	float n1,n2;
-	int op;
-	setlocale(LC_ALL,"");
-	printf("Informe um número: ");
-	scanf("%f",&n1);
-	printf("Informe outro número: ");
-	scanf("%f",&n2);
-	printf("Que operação deseja fazer (Digite 1 para Adição, 2 para subtração, 3 para multiplicação ou 4 para divisão): ");
-	scanf("%d",&op); 
+    float n1,n2;
+    int op;
+    setlocale(LC_ALL,"");
+    printf("Informe um nÃºmero: ");
+    scanf("%f",&n1);
+    printf("Informe outro nÃºmero: ");
+    scanf("%f",&n2);
+    printf("Que operaÃ§Ã£o deseja fazer (Digite 1 para AdiÃ§Ã£o, 2 para subtraÃ§Ã£o, 3 para multiplicaÃ§Ã£o ou 4 para divisÃ£o): ");
+    scanf("%d",&op); 
 	
     switch(op){
        case 1:{
          printf("%.2f + %.2f = %.2f\n",n1, n2, n1+n2);	
-	     break;}
-	   case 2:{
-	     printf("%.2f - %.2f = %.2f\n",n1, n2, n1-n2);	
-		 break;}
-	   case 3:{
-	     printf("%.2f * %.2f = %.2f\n",n1, n2, n1*n2);	
-		 break;}
-	   case 4:{
-	     printf("%.2f / %.2f = %.2f\n",n1, n2, n1/n2);	
-		 break;}
-	   default:{
-	   	  printf("Dados invalidos!\n");
-		  break;}
-   }    
+         break;}
+       case 2:{
+	 printf("%.2f - %.2f = %.2f\n",n1, n2, n1-n2);	
+	 break;}
+       case 3:{
+         printf("%.2f * %.2f = %.2f\n",n1, n2, n1*n2);	
+	 break;}
+       case 4:{
+         printf("%.2f / %.2f = %.2f\n",n1, n2, n1/n2);	
+	 break;}
+       default:{
+   	  printf("Dados invalidos!\n");
+	  break;}
+}    
 return 0;
 }
