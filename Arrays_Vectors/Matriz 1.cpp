74046d76da04ae5753e26b0f42aa3c4883eb0bd9
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-08 00:05:02 -0300

    Matriz 1

--- Arrays_Vectors/Matriz 1.cpp
+++ Arrays_Vectors/Matriz 1.cpp
@@ -6,23 +6,23 @@ main(){
 int mat[3][3],matB[3][3],i,j;
 
     for(i=0;i<3;i++){
-	for(j=0;j<3;j++){
-	setlocale(LC_ALL,"");
-	printf("Digite o valor: ");
-	scanf("%i",&mat[i][j]);
-	if((i==0 && j==0)||(i==0 && j==2)||(i==2 && j==0)||(i==2 && j==2))  (matB[i][j]=mat[i][j]*2);
-	if((i==0 && j==1)||(i==1 && j==0)||(i==1 && j==2)||(i==2 && j==1))  (matB[i][j]=mat[i][j]*3);	
-	if(i==1 && j==1)                                                    (matB[i][j]=0);
+    for(j=0;j<3;j++){
+    setlocale(LC_ALL,"");
+    printf("Digite o valor: ");
+    scanf("%i",&mat[i][j]);
+    if((i==0 && j==0)||(i==0 && j==2)||(i==2 && j==0)||(i==2 && j==2))  (matB[i][j]=mat[i][j]*2);
+    if((i==0 && j==1)||(i==1 && j==0)||(i==1 && j==2)||(i==2 && j==1))  (matB[i][j]=mat[i][j]*3);	
+    if(i==1 && j==1)                                                    (matB[i][j]=0);
 }
 }
     for(i=0;i<3;i++){
-	for(j=0;j<3;j++){
-	matB[1][1]+=mat[i][j];	
+    for(j=0;j<3;j++){
+    matB[1][1]+=mat[i][j];	
 }
 }
     for(i=0;i<3;i++){
-	for(j=0;j<3;j++){
-	printf("%i",matB[i][j]);	
+    for(j=0;j<3;j++){
+    printf("%i",matB[i][j]);	
 }   printf("\n");
 }
 getchar();
