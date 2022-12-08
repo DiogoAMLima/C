836727a03b3d6c0e67278628e12f0a105c525b0f
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 21:12:02 -0300

    Novo Salário

--- Math/Novo Salário.cpp
+++ Math/Novo Salário.cpp
@@ -3,11 +3,11 @@
 #include <locale.h>
 
 int main(){
-	float sal,novo_sal;
-	setlocale(LC_ALL,"");
-	printf("Informe o sal·rio do funcion·rio: ");
-	scanf("%f",&sal);
-	novo_sal=(sal*25/100)+sal;
-	printf("\nO novo sal·rio com aumento È: %.f",novo_sal);
-	return 0;
+    float sal,novo_sal;
+    setlocale(LC_ALL,"");
+    printf("Informe o sal√°rio do funcion√°rio: ");
+    scanf("%f",&sal);
+    novo_sal=(sal*25/100)+sal;
+    printf("\nO novo sal√°rio com aumento √©: %.f",novo_sal);
+    return 0;
 }
