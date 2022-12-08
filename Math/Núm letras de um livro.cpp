6e9b93890eaa5cef4394983ce5e64b46cf0ba42e
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 21:13:03 -0300

    Número de letras de um livro

--- Math/Núm letras de um livro.cpp
+++ Math/Núm letras de um livro.cpp
@@ -3,13 +3,13 @@
 #include<locale.h>
 
 int main(){
-	float npl,nlp,num_letras_livro;
-	setlocale(LC_ALL,"");
-	printf("Informe o número de páginas do livro: ");
-	scanf("%f",&npl);
-	printf("\nInforme o número de letras por página: ");
-	scanf("%f",&nlp);
-	num_letras_livro=(npl*nlp);
-	printf("\nNúmero de letras no livro é: %.f",num_letras_livro);
-	return 0;
+    float npl,nlp,num_letras_livro;
+    setlocale(LC_ALL,"");
+    printf("Informe o nÃºmero de pÃ¡ginas do livro: ");
+    scanf("%f",&npl);
+    printf("\nInforme o nÃºmero de letras por pÃ¡gina: ");
+    scanf("%f",&nlp);
+    num_letras_livro=(npl*nlp);
+    printf("\nNÃºmero de letras no livro Ã©: %.f",num_letras_livro);
+    return 0;
 }
