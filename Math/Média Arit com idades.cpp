4358ea3aee3b292fb8094de0224346950eaf5b30
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 21:01:15 -0300

    Média Aritmética com idades

--- Math/Média Arit com idades.cpp
+++ Math/Média Arit com idades.cpp
@@ -3,17 +3,17 @@
 #include <locale.h>
 
 int main(){
-	float id1,id2,id3,id4,media;
-	setlocale(LC_ALL,"");
-	printf("Informe a primeira idade: ");
-	scanf("%f",&id1);
-	printf("Informe a segunda idade: ");
-	scanf("%f",&id2);
-	printf("Informe a terceira idade: ");
-	scanf("%f",&id3);
-	printf("Informe a quarta idade: ");
-	scanf("%f",&id4);
-	media=(id1+id2+id3+id4)/4;
-	printf("A média de idades entre %.1f e %.1f e %.1f e %.1f é: %.1f",id1, id2, id3, id4, media);
-	return 0;
+    float id1,id2,id3,id4,media;
+    setlocale(LC_ALL,"");
+    printf("Informe a primeira idade: ");
+    scanf("%f",&id1);
+    printf("Informe a segunda idade: ");
+    scanf("%f",&id2);
+    printf("Informe a terceira idade: ");
+    scanf("%f",&id3);
+    printf("Informe a quarta idade: ");
+    scanf("%f",&id4);
+    media=(id1+id2+id3+id4)/4;
+    printf("A mÃ©dia de idades entre %.1f e %.1f e %.1f e %.1f Ã©: %.1f",id1, id2, id3, id4, media);
+    return 0;
 }
