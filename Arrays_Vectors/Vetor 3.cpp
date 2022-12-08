7eff3bcf526a6e91d647daa084b0fc4108086139
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-08 00:10:35 -0300

    Vetor 3

--- Arrays_Vectors/Vetor 3.cpp
+++ Arrays_Vectors/Vetor 3.cpp
@@ -5,34 +5,34 @@
 
 int main()
 {
-	int x[10],y[10],cont,cont2;
-	cont2=1;
-	setlocale(LC_ALL,"");
+    int x[10],y[10],cont,cont2;
+    cont2=1;
+    setlocale(LC_ALL,"");
     cont=1;
     for(cont=1;cont<=10;cont++)
 {
-    printf("Entre com o número %d: ",cont);
+    printf("Entre com o nÃºmero %d: ",cont);
     scanf("%d",&x[cont]);
 }
-   cont=1;
-   for(cont=1;cont<=10;cont++)
+    cont=1;
+    for(cont=1;cont<=10;cont++)
 {
-   printf("\nO %d número digitado tem valor %d\n",cont,x[cont]);
+    printf("\nO %d nÃºmero digitado tem valor %d\n",cont,x[cont]);
 }
-   cont=1;
-   for(cont=1;cont<=10;cont++)
+    cont=1;
+    for(cont=1;cont<=10;cont++)
 {
-   y[cont]=(x[cont]*x[cont]);
+    y[cont]=(x[cont]*x[cont]);
 }
-   cont=1;
+    cont=1;
 
 {
-   for(cont2=1;cont2<=10;cont2++)
+    for(cont2=1;cont2<=10;cont2++)
 {
-	printf("\nO quadrado de %.2d é %.2d \n",x[cont2],y[cont2]);
+    printf("\nO quadrado de %.2d Ã© %.2d \n",x[cont2],y[cont2]);
 }
 
 }
-   getch();
-   return 0;
+    getch();
+    return 0;
 }
