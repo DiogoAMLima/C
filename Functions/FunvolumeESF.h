a04d4457ca17f02d57a4e729d709628968e56802
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:53:41 -0300

    FunvolumeESF.h

--- Functions/FunvolumeESF.h
+++ Functions/FunvolumeESF.h
@@ -1,10 +1,10 @@
 float volesf(float x)
 {
-	float PI,V,R,volumeesf;
-	PI=3;
-	R=x;
-	V=(PI*4/3)*R*R*R;
-	volumeesf=V;
+    float PI,V,R,volumeesf;
+    PI=3;
+    R=x;
+    V=(PI*4/3)*R*R*R;
+    volumeesf=V;
 	
-	return volumeesf;
+    return volumeesf;
 }
