4b15b09f0d4c9aba460e4ca1c964985d22c85510
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 23:02:28 -0300

    Função Recursiva 4

--- Recursive function/Função Recursiva 4.cpp
+++ Recursive function/Função Recursiva 4.cpp
@@ -4,33 +4,33 @@
 
 float fibonacci(int N)
 {
-	int n;
-	n=N;
-	if(n == 0)
-	return 0;
-	else if(n == 1)
-	return 1;
-	else
-	return (fibonacci(n-1)+fibonacci(n-2));
+    int n;
+    n=N;
+    if(n == 0)
+    return 0;
+    else if(n == 1)
+    return 1;
+    else
+    return (fibonacci(n-1)+fibonacci(n-2));
 }
-	int main()
+    int main()
 {
-	int fib,i;
-	while(1)
+    int fib,i;
+    while(1)
 {
-	setlocale(LC_ALL,"");
-	printf("Digite um número para calcular a sequência e mostrar o elemento: ");
-	scanf("%d",&fib);
-	if(fib>= 0 && fib<50)
-	break;
-	printf("\nValor inválido. \n");
+    setlocale(LC_ALL,"");
+    printf("Digite um nÃºmero para calcular a sequÃªncia e mostrar o elemento: ");
+    scanf("%d",&fib);
+    if(fib>= 0 && fib<50)
+    break;
+    printf("\nValor invÃ¡lido. \n");
 }
-	printf("\n%d elemento da sequência: ",fib);
-	for(i=0;i<=fib;i++);
+    printf("\n%d elemento da sequÃªncia: ",fib);
+    for(i=0;i<=fib;i++);
 {
-	printf("\n\nO elemento %d corresponde ao valor %.0f da sequência de fibonacci\n",fib,fibonacci(fib));
+    printf("\n\nO elemento %d corresponde ao valor %.0f da sequÃªncia de fibonacci\n",fib,fibonacci(fib));
 	
 }
-	return 0;
+    return 0;
 }
 
