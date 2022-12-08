dc5a7897db300fc49aab9716ab0066b62b2a4e1a
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:07:14 -0300

    Valor de Compra com Desconto

--- Math/Valor de Compra com Desconto.cpp
+++ Math/Valor de Compra com Desconto.cpp
@@ -3,16 +3,16 @@
 #include <locale.h>
 
 int main(){
-	float vCompra;
-	setlocale(LC_ALL,"");
-	printf("Informe o valor de compra: ");
-	scanf("%f",&vCompra);
+    float vCompra;
+    setlocale(LC_ALL,"");
+    printf("Informe o valor de compra: ");
+    scanf("%f",&vCompra);
     if (vCompra>=5000){
     	printf("\nDesconto de 20%%: %.2f - %.2f = %.2f",vCompra, vCompra*0.20, vCompra-(vCompra*0.2));	
-	}
-	else{
-	if(vCompra<5000)
-	    printf("\nDesconto de 15%%: %.2f - %.2f = %.2f",vCompra, vCompra*0.15, vCompra-(vCompra*0.15));	
-	}
+     }
+    else{
+    if(vCompra<5000)
+	printf("\nDesconto de 15%%: %.2f - %.2f = %.2f",vCompra, vCompra*0.15, vCompra-(vCompra*0.15));	
+    }
     return 0;	
 }
