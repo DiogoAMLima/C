e3916dea72b078f6cc8fd91a0f84612126263b33
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 21:10:03 -0300

    Múltiplo de 5

---
+++ Math/Múltiplo de 5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	int i;
+	setlocale(LC_ALL,"");
+	for(i=1;i<=100;i++){
+		if(i%5==0){
+			printf("%d é múltiplo de 5\n",i);
+		}
+	}
+	return 0;
+}
