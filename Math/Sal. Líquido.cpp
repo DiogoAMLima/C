cccc2dedfef87338eb3cdc8de464819a723604d3
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 21:57:37 -0300

    Salário Líquido

--- Math/Sal. Líquido.cpp
+++ Math/Sal. Líquido.cpp
@@ -3,16 +3,16 @@
 
 int main(){
 	
-	float vHora,horas,sMin,salB,imp,salL;
-	setlocale(LC_ALL,"");
-	printf("Informe a quantidade de horas trabalhadas: ");
-	scanf("%f",&horas);
-	printf("\nInforme o valor do sal·rio mÌnimo: ");
-	scanf("%f",&sMin);
-	vHora=sMin*0.02;  // Valor da hora È igual ao sal·rio mÌnimo * 0.02 (2%)
-	salB=horas*vHora;
-	imp=salB*0.03;  // Imposto È igual ao sal·rio bruto * 0.03 (3%)
-	salL=salB-imp;  // Sal·rio lÌquido È igual ao bruto - imposto
-	printf("\nO sal·rio liquido È: %5.1f",salL);
-	return 0;
+    float vHora,horas,sMin,salB,imp,salL;
+    setlocale(LC_ALL,"");
+    printf("Informe a quantidade de horas trabalhadas: ");
+    scanf("%f",&horas);
+    printf("\nInforme o valor do sal√°rio m√≠nimo: ");
+    scanf("%f",&sMin);
+    vHora=sMin*0.02;  // Valor da hora √© igual ao sal√°rio m√≠nimo * 0.02 (2%)
+    salB=horas*vHora;
+    imp=salB*0.03;  // Imposto √© igual ao sal√°rio bruto * 0.03 (3%)
+    salL=salB-imp;  // Sal√°rio l√≠quido √© igual ao bruto - imposto
+    printf("\nO sal√°rio liquido √©: %5.1f",salL);
+    return 0;
 }
