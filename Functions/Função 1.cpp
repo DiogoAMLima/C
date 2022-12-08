3316f6cf39c3d8bf1582756d72d5be31fd00ad23
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:13:37 -0300

    Função 1

--- Functions/Função 1.cpp
+++ Functions/Função 1.cpp
@@ -6,9 +6,9 @@
 
 int main()
 {	
-	int C;
-	setlocale(LC_ALL,"");
-	printf("Informe um numero inteiro n„o negativo: ");
-	scanf("%d",&C);
-	printf("\nO valor de %d no fatorial È: %d",C,fat(C));
+    int C;
+    setlocale(LC_ALL,"");
+    printf("Informe um numero inteiro n√£o negativo: ");
+    scanf("%d",&C);
+    printf("\nO valor de %d no fatorial √©: %d",C,fat(C));
 }
