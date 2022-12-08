59beb7d7f89623e0edad7fa0fccb1a97197e0cca
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:03:41 -0300

    Soma de 4 Números

--- Math/Soma de 4 Núm.cpp
+++ Math/Soma de 4 Núm.cpp
@@ -2,17 +2,17 @@
 #include<locale.h>
 
 int main(){
-	setlocale(LC_ALL,"");
+    setlocale(LC_ALL,"");
     int s,n1, n2, n3, n4;
-    printf("Informe um número inteiro: ");
+    printf("Informe um nÃºmero inteiro: ");
     scanf("%d", &n1);
-    printf("\nInforme outro número inteiro: ");
+    printf("\nInforme outro nÃºmero inteiro: ");
     scanf("%d", &n2);
-    printf("\nInforme mais um número inteiro: ");
+    printf("\nInforme mais um nÃºmero inteiro: ");
     scanf("%d", &n3);
-    printf("\nInforme o último número inteiro: ");
+    printf("\nInforme o Ãºltimo nÃºmero inteiro: ");
     scanf("%d", &n4);
     s=n1+n2+n3+n4;
-    printf("A soma de %d + %d + %d + %d é: %d", n1,n2,n3,n4,s);
+    printf("A soma de %d + %d + %d + %d Ã©: %d", n1,n2,n3,n4,s);
     return 0;
 }
