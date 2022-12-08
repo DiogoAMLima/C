5d128dd984b17b022d89f2d3e0b917b014a4c5d8
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 23:01:14 -0300

    Função Recursiva 2

--- Recursive function/Função Recursiva 2.cpp
+++ Recursive function/Função Recursiva 2.cpp
@@ -6,24 +6,24 @@
 int somaCubos(int x,int y,int z)
 {
     int a=0, b=0, c=0, soma=0;
-	a=x;
-	b=y;
-	c=z;
-	if((a<0)||(b<0)||(c<0))
-	return 0;
-	else
-	return soma += (a*a*a)+(b*b*b)+(c*c*c);
+    a=x;
+    b=y;
+    c=z;
+    if((a<0)||(b<0)||(c<0))
+    return 0;
+    else
+    return soma += (a*a*a)+(b*b*b)+(c*c*c);
 }
 
 int main()
 {
-	int a,b,c,soma;
-	setlocale(LC_ALL,"");
-	printf("Digite um valor: ");
-	scanf("%d",&a);
-	printf("\nDigite outro valor: ");
-	scanf("%d",&b);
-	printf("\nDigite mais um valor: ");
-	scanf("%d",&c);	
-	printf("\nO resultado da soma dos cubos de %d, %d e %d é: %d ",a, b, c, somaCubos(a,b,c));
+    int a,b,c,soma;
+    setlocale(LC_ALL,"");
+    printf("Digite um valor: ");
+    scanf("%d",&a);
+    printf("\nDigite outro valor: ");
+    scanf("%d",&b);
+    printf("\nDigite mais um valor: ");
+    scanf("%d",&c);	
+    printf("\nO resultado da soma dos cubos de %d, %d e %d Ã©: %d ",a, b, c, somaCubos(a,b,c));
 }
