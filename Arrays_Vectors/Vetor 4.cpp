bbefbed3edcc97398c5f1861456daf1fec2be274
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-08 00:11:23 -0300

    Vetor 4

--- Arrays_Vectors/Vetor 4.cpp
+++ Arrays_Vectors/Vetor 4.cpp
@@ -4,31 +4,31 @@
 
 int main()
 {
-	int x[10], i=0;
-	setlocale(LC_ALL,"");
-	printf("Digite o valor do vetor na posição x[0]: ");
-	scanf("%d",&x[0]);
-	printf("\nDigite o valor do vetor na posição x[1]: ");
-	scanf("%d",&x[1]);
-	printf("\nDigite o valor do vetor na posição x[2]: ");
-	scanf("%d",&x[2]);
-	printf("\nDigite o valor do vetor na posição x[3]: ");
-	scanf("%d",&x[3]);
-	printf("\nDigite o valor do vetor na posição x[4]: ");
-	scanf("%d",&x[4]);
-	printf("\nDigite o valor do vetor na posição x[5]: ");
-	scanf("%d",&x[5]);
-	printf("\nDigite o valor do vetor na posição x[6]: ");
-	scanf("%d",&x[6]);
-	printf("\nDigite o valor do vetor na posição x[7]: ");
-	scanf("%d",&x[7]);
-	printf("\nDigite o valor do vetor na posição x[8]: ");
-	scanf("%d",&x[8]);
-	printf("\nDigite o valor do vetor na posição x[9]: ");
-	scanf("%d",&x[9]);
-	for(i=0;i<10;i++)
-	{
-	system("color 4");
-	printf("\nO valor da posição %d é: %d",i,x[i]);	
-	}
+    int x[10], i=0;
+    setlocale(LC_ALL,"");
+    printf("Digite o valor do vetor na posiÃ§Ã£o x[0]: ");
+    scanf("%d",&x[0]);
+    printf("\nDigite o valor do vetor na posiÃ§Ã£o x[1]: ");
+    scanf("%d",&x[1]);
+    printf("\nDigite o valor do vetor na posiÃ§Ã£o x[2]: ");
+    scanf("%d",&x[2]);
+    printf("\nDigite o valor do vetor na posiÃ§Ã£o x[3]: ");
+    scanf("%d",&x[3]);
+    printf("\nDigite o valor do vetor na posiÃ§Ã£o x[4]: ");
+    scanf("%d",&x[4]);
+    printf("\nDigite o valor do vetor na posiÃ§Ã£o x[5]: ");
+    scanf("%d",&x[5]);
+    printf("\nDigite o valor do vetor na posiÃ§Ã£o x[6]: ");
+    scanf("%d",&x[6]);
+    printf("\nDigite o valor do vetor na posiÃ§Ã£o x[7]: ");
+    scanf("%d",&x[7]);
+    printf("\nDigite o valor do vetor na posiÃ§Ã£o x[8]: ");
+    scanf("%d",&x[8]);
+    printf("\nDigite o valor do vetor na posiÃ§Ã£o x[9]: ");
+    scanf("%d",&x[9]);
+    for(i=0;i<10;i++)
+    {
+    system("color 4");
+    printf("\nO valor da posiÃ§Ã£o %d Ã©: %d",i,x[i]);	
+    }
 }
