f4b484fc1db62324e7053594eae60bf7e7713678
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 23:03:09 -0300

    Função Recursiva 5

--- Recursive function/Função Recursiva 5.cpp
+++ Recursive function/Função Recursiva 5.cpp
@@ -4,19 +4,19 @@
 
 int C(int N)
 {
-	int n;
-	n=N;
-	if(n == 0)
-	return 1;
-	else if(n>0)
-	return 2*(2*(n-1)) + C((n-1)/(n+1));
+    int n;
+    n=N;
+    if(n == 0)
+    return 1;
+    else if(n>0)
+    return 2*(2*(n-1)) + C((n-1)/(n+1));
 	
 }
 int main()
 {
-	int n;
-	setlocale(LC_ALL,"");
-	printf("Digite o valor de n: ");
-	scanf("%d",&n);
-	printf("\nO resultado é: %.0d",C(n));
+    int n;
+    setlocale(LC_ALL,"");
+    printf("Digite o valor de n: ");
+    scanf("%d",&n);
+    printf("\nO resultado Ã©: %.0d",C(n));
 }
