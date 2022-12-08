4db700ba3a1ced9f62ea8ee0784b111e893b1470
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:32:53 -0300

    Função 13

--- Functions/Função_13.cpp
+++ Functions/Função_13.cpp
@@ -4,29 +4,29 @@
 
 int alg(int x,int y, int z)
 {
-	int aux,num,soma,alg1;
-	aux=x;
-	num=y;
-	soma=z;
-	return alg1;
+    int aux,num,soma,alg1;
+    aux=x;
+    num=y;
+    soma=z;
+    return alg1;
 }
 
 int main()
 {
-	int num,aux,soma=0;
-	setlocale(LC_ALL,"");
-	printf("Informe um número: ");
-	scanf("%d",&num);
-	if(num>0)
-	{
-	aux=num;
-	while(aux>0)
-	{
-	soma+=aux%10;
-	aux/=10;
-	}
+    int num,aux,soma=0;
+    setlocale(LC_ALL,"");
+    printf("Informe um nÃºmero: ");
+    scanf("%d",&num);
+    if(num>0)
+    {
+    aux=num;
+    while(aux>0)
+    {
+    soma+=aux%10;
+    aux/=10;
+    }
 }
-	else
-	printf("Número inválido");
-	printf("\nSoma dos algarismos de %d = %d\n",num,soma,alg(num,aux,soma));
+    else
+    printf("NÃºmero invÃ¡lido");
+    printf("\nSoma dos algarismos de %d = %d\n",num,soma,alg(num,aux,soma));
 }
