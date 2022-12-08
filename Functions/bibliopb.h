ea0fc2089bf3757e6b9529563c24b1ae49ed8d64
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:44:14 -0300

    bibliopb.h

--- Functions/bibliopb.h
+++ Functions/bibliopb.h
@@ -1,9 +1,8 @@
 int pot(int base,int expoente)
 {
-	int pot1,i;
-	pot1=1;
-	for(i=1;i<=expoente;i++)
-	pot1*=base;
-	return pot1;
-
+    int pot1,i;
+    pot1=1;
+    for(i=1;i<=expoente;i++)
+    pot1*=base;
+    return pot1;
 }
