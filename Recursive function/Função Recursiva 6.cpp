de8c2af03fb295e4ee6be93c4c7ad2e45a1e666f
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 23:04:14 -0300

    Função Recursiva 6

--- Recursive function/Função Recursiva 6.cpp
+++ Recursive function/Função Recursiva 6.cpp
@@ -4,23 +4,23 @@
 
 int mult(int A,int B)
 {
-	int a,b;
-	a=A;
-	b=B;
-	if(a == 0 || b == 0)
-	return 0;
-	else if (b == 1)
-	return a;
-	else
-	return a+mult(a,b-1);
+    int a,b;
+    a=A;
+    b=B;
+    if(a == 0 || b == 0)
+    return 0;
+    else if (b == 1)
+    return a;
+    else
+    return a+mult(a,b-1);
 }
 int main()
 {
-	int a,b;
-	setlocale(LC_ALL,"");
-	printf("Digite o valor de a: ");
-	scanf("%d",&a);
-	printf("Digite o valor de b: ");
-	scanf("%d",&b);
-	printf("\nO resultado é: %d",a+mult(a,b-1));
+    int a,b;
+    setlocale(LC_ALL,"");
+    printf("Digite o valor de a: ");
+    scanf("%d",&a);
+    printf("Digite o valor de b: ");
+    scanf("%d",&b);
+    printf("\nO resultado Ã©: %d",a+mult(a,b-1));
 }
