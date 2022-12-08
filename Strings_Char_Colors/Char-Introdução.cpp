b828f76d50c7aada7537c214a50b9d348dad030a
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 23:19:24 -0300

    Char-Introdução

--- Strings_Char_Colors/Char-Introdução.cpp
+++ Strings_Char_Colors/Char-Introdução.cpp
@@ -10,17 +10,17 @@
    char Nome2[30];
    
    setlocale(LC_ALL,"");
-   strcpy(Nome, "Diogo de Albuquerque");  // scrpy = funÁ„o para copiar o valor da string na segunda posiÁ„o do argumento (segundo argumento pode ser: express„o em string, vari·vel ou valor literal de uma string entre "")
+   strcpy(Nome, "Diogo de Albuquerque");  // scrpy = fun√ß√£o para copiar o valor da string na segunda posi√ß√£o do argumento (segundo argumento pode ser: express√£o em string, vari√°vel ou valor literal de uma string entre aspas "")
    
-   printf("O funcion·rio %s foi transferido", Nome);
+   printf("O funcion√°rio %s foi transferido", Nome);
    
    printf("\n\nInforme um nome: ");
-   gets(Nome2);  // N„o muito recomendado porque n„o checa a tamanho declarado na vari·vel, neste caso [30].
+   gets(Nome2);  // N√£o muito recomendado porque n√£o checa a tamanho declarado na vari√°vel, neste caso [30].
    
-   printf("\nO funcion·rio: %s foi promovido\n", Nome2);
+   printf("\nO funcion√°rio: %s foi promovido\n", Nome2);
    
    // Lembrando que strings em c terminam no \0 (Strings = vetor de caracteres)
    
-   getch();  //Usado para pausar o terminal. Esta funÁ„o lÍ um caracter do teclado e n„o mostra no terminal
-   // Temos a funÁ„o clrscr() para limpar a tela
+   getch();  //Usado para pausar o terminal. Esta fun√ß√£o l√™ um caracter do teclado e n√£o mostra no terminal
+   // Temos a fun√ß√£o clrscr() para limpar a tela
 }
