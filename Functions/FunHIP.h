37680df9c47fcc0a4eaa9bb7d451c1790c6dadaf
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:49:00 -0300

    FunHIP.h

--- Functions/FunHIP.h
+++ Functions/FunHIP.h
@@ -1,8 +1,8 @@
 float hip(float x,float y)
 {
-	float a,b,hipotenusa;
-	a=x;
-	b=y;
-	hipotenusa=(a*a)+(b*b);
-	return hipotenusa;
+    float a,b,hipotenusa;
+    a=x;
+    b=y;
+    hipotenusa=(a*a)+(b*b);
+    return hipotenusa;
 }
