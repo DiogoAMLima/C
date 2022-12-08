01a7537320e806fa375d4463ae7bffc2c929dca2
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:49:48 -0300

    FunOperadores.h

--- Functions/FunOperadores.h
+++ Functions/FunOperadores.h
@@ -1,11 +1,11 @@
 float oper(float x,float y)
 {
-	float num1,num2;
-	char op,sair;
-	num1=x;
-	num2=y;
+    float num1,num2;
+    char op,sair;
+    num1=x;
+    num2=y;
 	
-	return op;
+    return op;
 }
 
 
