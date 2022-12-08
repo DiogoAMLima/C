41c33017939a4a3c3046fcd5cab3922e1673281e
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 21:02:34 -0300

    Média Ponderada

--- Math/Média Ponderada.cpp
+++ Math/Média Ponderada.cpp
@@ -3,21 +3,21 @@
 #include <locale.h>
 
 int main(){
-	float n1,n2,n3,p1,p2,p3,media;
-	setlocale(LC_ALL,"");
-	printf("Informe a primeira nota: ");
-	scanf("%f",&n1);
-	printf("Informe o peso da primeira nota: ");
-	scanf("%f",&p1);
-	printf("Informe a segunda nota: ");
-	scanf("%f",&n2);
-	printf("Informe o peso da segunda nota: ");
-	scanf("%f",&p2);
-	printf("Informe a terceira nota: ");
-	scanf("%f",&n3);
-	printf("Informe o peso da terceira nota: ");
-	scanf("%f",&p3);
-	media=(n1*p1+n2*p2+n3*p3)/(p1+p2+p3);
-	printf("A média ponderada entre %.1f e %.1f e %.1f é: %.1f",n1, n2, n3, media);
-	return 0;
+    float n1,n2,n3,p1,p2,p3,media;
+    setlocale(LC_ALL,"");
+    printf("Informe a primeira nota: ");
+    scanf("%f",&n1);
+    printf("Informe o peso da primeira nota: ");
+    scanf("%f",&p1);
+    printf("Informe a segunda nota: ");
+    scanf("%f",&n2);
+    printf("Informe o peso da segunda nota: ");
+    scanf("%f",&p2);
+    printf("Informe a terceira nota: ");
+    scanf("%f",&n3);
+    printf("Informe o peso da terceira nota: ");
+    scanf("%f",&p3);
+    media=(n1*p1+n2*p2+n3*p3)/(p1+p2+p3);
+    printf("A mÃ©dia ponderada entre %.1f e %.1f e %.1f Ã©: %.1f",n1, n2, n3, media);
+    return 0;
 }
