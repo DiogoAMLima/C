254e6ebb67f942884f15758d843afe04203592a2
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 21:56:16 -0300

    Rendimento

--- Math/Rendimento.cpp
+++ Math/Rendimento.cpp
@@ -3,15 +3,15 @@
 
 int main(){
 	
-	float valor,tx, rend, vTotal;
-	setlocale(LC_ALL,"");
-	printf("Informe o valor do depósito: ");
-	scanf("%f",&valor);
-	printf("Informe o valor da taxa de juros (%%): ");
-	scanf("%f",&tx);
-	rend=valor*tx/100;
-	vTotal=valor+rend;
-	printf("O rendimento é: %5.1f",rend);
-	printf("\nO valor final é: %5.1f",vTotal);
-	return 0;
+    float valor,tx, rend, vTotal;
+    setlocale(LC_ALL,"");
+    printf("Informe o valor do depÃ³sito: ");
+    scanf("%f",&valor);
+    printf("Informe o valor da taxa de juros (%%): ");
+    scanf("%f",&tx);
+    rend=valor*tx/100;
+    vTotal=valor+rend;
+    printf("O rendimento Ã©: %5.1f",rend);
+    printf("\nO valor final Ã©: %5.1f",vTotal);
+    return 0;
 }
