ff7775f70c64d0e933cb484883566941ff3e87c7
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 23:07:42 -0300

    Função Recursiva 8

--- Recursive function/Função Recursiva 8.cpp
+++ Recursive function/Função Recursiva 8.cpp
@@ -6,12 +6,12 @@ void print(int N);
 
 main()
 {
-	print(5);
+    print(5);
 }
 void print (int N)
 {
-	if(N>=1){
-		printf(" %d",N);
-		print(N-1);
-	}
+    if(N>=1){
+        printf(" %d",N);
+        print(N-1);
+    }
 }
