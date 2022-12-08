756e482d9a6a4e168b20eb82faea9bd41ca2c5c6
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:06:05 -0300

    Tipo de Triângulo

--- Math/Tipo de Triang..cpp
+++ Math/Tipo de Triang..cpp
@@ -2,23 +2,23 @@
 #include <locale.h>
 
 int main(){
-	float x,y,z;
-	setlocale(LC_ALL,"");
-	printf("Informe o lado x: ");
-	scanf("%f",&x);
-	printf("Informe o lado y: ");
-	scanf("%f",&y);
-	printf("Informe o lado z: ");
-	scanf("%f",&z);
-	if((x<y+z)&&(y<x+z)&&(z<x+y)) // Condição para ser triângulo
-	if((x==y)&&(y==z))
-	printf("\nTRIÂNGULO EQUILÁTERO\n");
-	else
-	if((x!=y)&&(y!=z))
-	printf("\nTRIÂNGULO ESCALENO\n");
-	else
-	printf("\nTRIÂNGULO ISÓSCELES\n");
-	else
-	printf("\nOs lados não formam um triângulo...\n");
+    float x,y,z;
+    setlocale(LC_ALL,"");
+    printf("Informe o lado x: ");
+    scanf("%f",&x);
+    printf("Informe o lado y: ");
+    scanf("%f",&y);
+    printf("Informe o lado z: ");
+    scanf("%f",&z);
+    if((x<y+z)&&(y<x+z)&&(z<x+y)) // CondiÃ§Ã£o para ser triÃ¢ngulo
+    if((x==y)&&(y==z))
+    printf("\nTRIÃ‚NGULO EQUILÃTERO\n");
+    else
+    if((x!=y)&&(y!=z))
+    printf("\nTRIÃ‚NGULO ESCALENO\n");
+    else
+    printf("\nTRIÃ‚NGULO ISÃ“SCELES\n");
+    else
+    printf("\nOs lados nÃ£o formam um triÃ¢ngulo...\n");
    return 0;		
 }
