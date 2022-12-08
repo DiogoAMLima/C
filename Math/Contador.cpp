8d3273a3407750f04a99ebab89295ff090fafc75
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 21:00:23 -0300

    Contador

--- Math/Contador.cpp
+++ Math/Contador.cpp
@@ -2,11 +2,11 @@
 #include<stdlib.h>
 
 int main(){
-	int cont;
-	for(cont=1;cont<=200;cont++){
-		printf("%d\n",cont);
-		printf("\n");
-	}
-	system("pause");
-	return 0;
+    int cont;
+    for(cont=1;cont<=200;cont++){
+	    printf("%d\n",cont);
+	    printf("\n");
+    }
+    system("pause");
+    return 0;
 }
