44b0b3f42ddb683968a5b526eb9d041bfeec5153
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 23:26:07 -0300

    String 5

--- Strings_Char_Colors/String 5.cpp
+++ Strings_Char_Colors/String 5.cpp
@@ -6,19 +6,19 @@
 
 main()
 {
-	char nome[30],sexo;
-	int i;
-	setlocale (LC_ALL, "");
-	printf("Verifica se o sexo é feminino [f/F]...\n\n");
-	printf("Digite um nome: ");
-	gets(nome);
-	printf("\nDigite o sexo da pessoa: ");
-	scanf("%c",&sexo);
-	printf("\nDigite a idade da pessoa: ");
-	scanf("%d",&i);
-	if(sexo=='f'|| sexo=='F' && i<25)
-	printf("\nAceito\n\n",nome);
-	else
-	printf("\nNão aceito\n\n");
-	system("pause");
+    char nome[30],sexo;
+    int i;
+    setlocale (LC_ALL, "");
+    printf("Verifica se o sexo Ã© feminino [f/F]...\n\n");
+    printf("Digite um nome: ");
+    gets(nome);
+    printf("\nDigite o sexo da pessoa: ");
+    scanf("%c",&sexo);
+    printf("\nDigite a idade da pessoa: ");
+    scanf("%d",&i);
+    if(sexo=='f'|| sexo=='F' && i<25)
+    printf("\nAceito\n\n",nome);
+    else
+    printf("\nNÃ£o aceito\n\n");
+    system("pause");
 }
