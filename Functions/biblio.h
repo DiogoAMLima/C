3a5da28bced5a1aad56ac3e5c526e32e7d139378
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:43:45 -0300

    biblio.h

--- Functions/biblio.h
+++ Functions/biblio.h
@@ -1,11 +1,11 @@
 int fat(int x)
 {
-	int i,fat1;
-	i=x;
-	fat1=1;
-	while(i>1){
-	fat1*=i;
-	i-=1;		
-	}
-	return fat1;
+    int i,fat1;
+    i=x;
+    fat1=1;
+    while(i>1){
+    fat1*=i;
+    i-=1;		
+    }
+    return fat1;
 }
