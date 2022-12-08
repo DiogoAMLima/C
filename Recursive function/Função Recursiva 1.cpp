029d413b92680e60ae7e0fca9ac90f88f70280ad
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 23:00:24 -0300

    Função Recursiva 1

--- Recursive function/Função Recursiva 1.cpp
+++ Recursive function/Função Recursiva 1.cpp
@@ -5,17 +5,17 @@
 
 int fat(int N)
 {
-	if (N < 2)
-	return 1;
-	else
+    if (N < 2)
+    return 1;
+    else
     return N*fat(N-1);
 }
 
 int main()
 {
-	int N;
-	setlocale(LC_ALL,"");
-	printf("Digite um num: ");
-	scanf("%d",&N);
-	printf("\nO resultado é: %d ",fat(N));
+    int N;
+    setlocale(LC_ALL,"");
+    printf("Digite um num: ");
+    scanf("%d",&N);
+    printf("\nO resultado Ã©: %d ",fat(N));
 }
