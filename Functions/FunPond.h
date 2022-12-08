bc2e53b3d2b2cfa90491399f7ccf08bc3027158b
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-07 22:50:31 -0300

    FunPond.h

--- Functions/FunPond.h
+++ Functions/FunPond.h
@@ -1,11 +1,9 @@
 float pond(float x,float y,float z)
 {
-	float A,D,E,F,pond1;
-	D=x;
-	E=y;
-	F=z;
-	pond1=(D,E,F);
-	return pond1;
+    float A,D,E,F,pond1;
+    D=x;
+    E=y;
+    F=z;
+    pond1=(D,E,F);
+    return pond1;
 }
-
-	
