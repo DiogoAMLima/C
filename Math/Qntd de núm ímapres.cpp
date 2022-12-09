57f60781740ff8b467cab80a273da6eaf8ae3a25
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-08 21:20:49 -0300

    Quantidade de Números Ímpares

---
+++ Math/Qntd de núm ímapres.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){
+	setlocale(LC_ALL,"");
+	int cont,num,impar;
+	impar=0;
+	for(cont=1;cont<=10;cont++){
+		 printf("informe o número: ");
+		 scanf("%d",&num);
+		 if(num%2==1)
+		 impar++;
+	}
+	printf("quantidade de números ímpares:%d",impar);
+	return 0;
+}
