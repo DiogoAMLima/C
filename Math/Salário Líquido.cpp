da79ee3e43fab2c3681633ebd4ee49e6e45952ea
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-08 21:24:01 -0300

    Salário Líquido 2

--- Math/Salário Líquido.cpp
+++ Math/Salário Líquido.cpp
@@ -3,13 +3,13 @@
 #include <locale.h>
 
 int main(){
-	int sal_bruto,sal_liquido,inss,irf;
-	setlocale(LC_ALL,"");
-	printf("Informe o sal·rio bruto: ");
-	scanf("%d",&sal_bruto);
-	inss=sal_bruto*0.11;  // Valor do inss È igual a sal·rio bruto * 0.11 (11%)
-	irf=(sal_bruto-inss)*0.23;  // Imposto de renda È igual a (al·rio bruto - inss) * 0.23 (23%)
-	sal_liquido=sal_bruto-inss-irf;  // Sal·rio lÌquido È igual a sal·rio bruto - inss - irf
-	printf("\nO sal·rio lÌquido È: %d",sal_liquido);  // %d = valor inteiro decimal (Se digitar 2000 como sal·rio bruto, termos um resultado de 1371, quando na verdade o resultado seria 1370,6)
-	return 0;
+    int sal_bruto,sal_liquido,inss,irf;
+    setlocale(LC_ALL,"");
+    printf("Informe o sal√°rio bruto: ");
+    scanf("%d",&sal_bruto);
+    inss=sal_bruto*0.11;  // Valor do inss √© igual a sal√°rio bruto * 0.11 (11%)
+    irf=(sal_bruto-inss)*0.23;  // Imposto de renda √© igual a (al√°rio bruto - inss) * 0.23 (23%)
+    sal_liquido=sal_bruto-inss-irf;  // Sal√°rio l√≠quido √© igual a sal√°rio bruto - inss - irf
+    printf("\nO sal√°rio l√≠quido √©: %d",sal_liquido);  // %d = valor inteiro decimal (Se digitar 2000 como sal√°rio bruto, termos um resultado de 1371, quando na verdade o resultado seria 1370,6)
+    return 0;
 }
