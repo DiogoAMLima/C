dcfd6c424c803f7d8d469e760bb0d4f758b87084
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-08 21:14:10 -0300

    Desafio 4

--- Challenges/Desafio4.cpp
+++ Challenges/Desafio4.cpp
@@ -3,14 +3,14 @@
 #include <locale.h>
 
 int main(){
-	int x,soma=0;
-	setlocale(LC_ALL,"");
-	printf("Digito de valores até a soma ser 5000 ou superior...\n");
-	do{
-	printf("\nDigite um valor: ");
-	scanf("%d",&x);
-  	soma=soma+x;
-}  	while(soma<5000);
-	printf("\nO resultado final é: %d",soma);
-	return 0;
+    int x,soma=0;
+    setlocale(LC_ALL,"");
+    printf("Digito de valores atÃ© a soma ser 5000 ou superior...\n");
+    do{
+    printf("\nDigite um valor: ");
+    scanf("%d",&x);
+    soma=soma+x;
+}   while(soma<5000);
+    printf("\nO resultado final Ã©: %d",soma);
+    return 0;
 } 
