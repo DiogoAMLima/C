b509b3481348984ec29e69a496637a7cd6c9e871
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-08 21:15:06 -0300

    Desafio 5

--- Challenges/Desafio5.cpp
+++ Challenges/Desafio5.cpp
@@ -3,32 +3,32 @@
 #include <locale.h>
 
 int main(){
-	int n1,n2,n3,n4,somaN;
-	setlocale(LC_ALL,"");
-	printf("————SOMA DE NÚMEROS NEGATIVOS————\n");
-	printf("\nDigite um num: ");
-	scanf("%d",&n1);
-	printf("Digite um num: ");
-	scanf("%d",&n2);
-	printf("Digite um num: ");
-	scanf("%d",&n3);
-	printf("Digite um num: ");
-	scanf("%d",&n4);
-	if(n1>0){
-	n1=0;
+    int n1,n2,n3,n4,somaN;
+    setlocale(LC_ALL,"");
+    printf("â€”â€”â€”â€”SOMA DE NÃšMEROS NEGATIVOSâ€”â€”â€”â€”\n");
+    printf("\nDigite um num: ");
+    scanf("%d",&n1);
+    printf("Digite um num: ");
+    scanf("%d",&n2);
+    printf("Digite um num: ");
+    scanf("%d",&n3);
+    printf("Digite um num: ");
+    scanf("%d",&n4);
+    if(n1>0){
+    n1=0;
 }
-	if(n2>0){
-	n2=0;
+    if(n2>0){
+    n2=0;
 }
-	if(n3>0){
-	n3=0;
+    if(n3>0){
+    n3=0;
 }
     if(n4>0){
     n4=0;	
 }
-	if((n1<0)||(n2<0)||(n3<0)||(n4<0)){
-	somaN=n1+n2+n3+n4;
+    if((n1<0)||(n2<0)||(n3<0)||(n4<0)){
+    somaN=n1+n2+n3+n4;
 }
-	printf("\nA soma dos números negativos é: %d",somaN);
-	return 0;
+    printf("\nA soma dos nÃºmeros negativos Ã©: %d",somaN);
+    return 0;
 }
