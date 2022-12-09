5169ada6fa2e1317921bdcbd567b70d5a44f7d01
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-08 21:17:56 -0300

    Desafio 10

--- Challenges/Desafio_10.cpp
+++ Challenges/Desafio_10.cpp
@@ -3,21 +3,21 @@
 #include <locale.h>
 
 int main(){
-	int a,b,c,*pnt,*pnt2,*pnt3;
-	setlocale(LC_ALL,"");
-	printf("Digite o valor de a: ");
-	scanf("%d",&a);
-	printf("\nDigite o valor de b: ");
-	scanf("%d",&b);
-	printf("\nDigite o valor de c: ");
-	scanf("%d",&c);
-	pnt=&a;
-	*pnt=a;
-	pnt2=&b;
-	*pnt2=b;
-	pnt3=&c;
-	*pnt3=c;
-	printf("\nValor dos ponteiros: %d-%d-%d",*pnt,*pnt2,*pnt3);
-	printf("\nValores do endereços de memória: %d-%d-%d\n",pnt,pnt2,pnt3);
-	return 0;
+    int a,b,c,*pnt,*pnt2,*pnt3;
+    setlocale(LC_ALL,"");
+    printf("Digite o valor de a: ");
+    scanf("%d",&a);
+    printf("\nDigite o valor de b: ");
+    scanf("%d",&b);
+    printf("\nDigite o valor de c: ");
+    scanf("%d",&c);
+    pnt=&a;
+    *pnt=a;
+    pnt2=&b;
+    *pnt2=b;
+    pnt3=&c;
+    *pnt3=c;
+    printf("\nValor dos ponteiros: %d-%d-%d",*pnt,*pnt2,*pnt3);
+    printf("\nValores do endereÃ§os de memÃ³ria: %d-%d-%d\n",pnt,pnt2,pnt3);
+    return 0;
 }
