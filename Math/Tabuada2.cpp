da8e0d99e508a793616bf04778228b36abe0c4d9
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-10 10:39:10 -0300

    Tabuada 2

---
+++ Math/Tabuada2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	int cont=0,num,resposta;
+	do
+	{
+	    system("cls");
+		setlocale(LC_ALL,"");
+		printf("-----Tabuada-----\n\n");
+		printf("Informe um número: ");
+		scanf("%d",&num);
+		for(cont=0;cont<=10;cont++)
+	{
+		printf("\n%d x %d = %d\n",num,cont,num*cont);
+	}
+	printf("\nDigite 1 para sair ou qualquer outro número para fazer um novo cálculo: ");
+	scanf("%d",&resposta);
+	}while(resposta != 1);
+	return 0;
+}
