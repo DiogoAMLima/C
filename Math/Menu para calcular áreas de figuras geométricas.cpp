ba6406bca9f68064fca849c4b8ea1b9c152abf10
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-11 15:14:30 -0300

    Menu para Calcular Áreas de Figuras Geométricas

--- Math/Menu para calcular áreas de figuras geométricas.cpp
+++ Math/Menu para calcular áreas de figuras geométricas.cpp
@@ -7,107 +7,105 @@ float a,b,h,B,base,l,pi,r;
 
 float MenuToggle(){
 	
-	setlocale(LC_ALL,"");
-	printf("\n---Escolha uma opÁ„o---\n");
-	printf("\n\n(1) Para calcular a ·rea do tri‚ngulo");
-	printf("\n(2) Para calcular a ·rea da circunferÍncia");
-	printf("\n(3) Para calcular a ·rea do quadrado"); 
-	printf("\n(4) Para calcular a ·rea do trapÈzio");
-	printf("\n(5) Para sair");
-	return 0;
+    setlocale(LC_ALL,"");
+    printf("\n---Escolha uma op√ß√£o---\n");
+    printf("\n\n(1) Para calcular a √°rea do tri√¢ngulo");
+    printf("\n(2) Para calcular a √°rea da circunfer√™ncia");
+    printf("\n(3) Para calcular a √°rea do quadrado"); 
+    printf("\n(4) Para calcular a √°rea do trap√©zio");
+    printf("\n(5) Para sair");
+    return 0;
 }
 	
 float AreaTriangulo(){
 	
 	
-	printf("\nDigite a altura: ");
-	scanf("%f",&h);
-	printf("\nDigite a base: ");
-	scanf("%f",&b);
-	a=b*h/2;
-	return (a);
+    printf("\nDigite a altura: ");
+    scanf("%f",&h);
+    printf("\nDigite a base: ");
+    scanf("%f",&b);
+    a=b*h/2;
+    return (a);
 }
 
 float AreaCircunferencia(){
 	
 	
-	printf("\nDigite o raio: ");
-	scanf("%f",&r);
+    printf("\nDigite o raio: ");
+    scanf("%f",&r);
 	
-	a=3.14*(r*r);
+    a=3.14*(r*r);
 	
-	return (a);
+    return (a);
 
 }
 
 float AreaQuadrado(){
 	
 	
-	printf("\n\nDigite o valor do lado do quadrado: ");
-	scanf("%f",&l);
+    printf("\n\nDigite o valor do lado do quadrado: ");
+    scanf("%f",&l);
 	
-	a=l*l;
+    a=l*l;
 	
-	return (a); 
+    return (a); 
 	
 }
 
 float AreaTrapezio(){
 	
 	
-	printf("\nDigite o valor da base menor: ");
-	scanf("%f",&b);
-	printf("\nDigite o valor da base maior: ");
-	scanf("%f",&B);
-	printf("\nDigite o valor da altura: ");
-	scanf("%f",&h);
+    printf("\nDigite o valor da base menor: ");
+    scanf("%f",&b);
+    printf("\nDigite o valor da base maior: ");
+    scanf("%f",&B);
+    printf("\nDigite o valor da altura: ");
+    scanf("%f",&h);
 	
-	a=(B+b)*h/2;
+    a=(B+b)*h/2;
 		
- 	return (a);
+    return (a);
 }
 	
 int main(){
 	
  do{
- 	MenuToggle(); 
- 	printf("\n\nDigite sua opÁ„o: ");
- 	scanf("%d",&op);
+    MenuToggle(); 
+    printf("\n\nDigite sua op√ß√£o: ");
+    scanf("%d",&op);
  	
- 	switch(op){
+    switch(op){
  		
- 		case 1:{
- 			AreaTriangulo();
- 			printf("\n¡rea tri‚ngulo: %.2f\n",a);
- 			break;
+ 	case 1:{
+ 	   AreaTriangulo();
+ 	   printf("\n√Årea tri√¢ngulo: %.2f\n",a);
+ 	   break;
  		}
- 		case 2:{
- 			AreaCircunferencia();
- 			printf("\n¡rea circunferÍncia: %.2f\n",a);
- 			break;
+ 	case 2:{
+ 	   AreaCircunferencia();
+ 	   printf("\n√Årea circunfer√™ncia: %.2f\n",a);
+ 	   break;
  		}
- 		case 3:{
- 			AreaQuadrado();
- 			printf("\n¡rea quadrado: %.2f\n",a);
- 			break;
+ 	case 3:{
+ 	   AreaQuadrado();
+ 	   printf("\n√Årea quadrado: %.2f\n",a);
+ 	   break;
  		}
- 		case 4:{
- 			AreaTrapezio();
- 			printf("\n¡rea trapÈzio: %.2f\n",a);
- 			break;
+ 	case 4:{
+ 	   AreaTrapezio();
+ 	   printf("\n√Årea trap√©zio: %.2f\n",a);
+ 	   break;
  		}
- 		case 5:{
- 			printf("\nAtÈ a prÛxima...\n\n");
-			break;
+ 	case 5:{
+ 	   printf("\nAt√© a pr√≥xima...\n\n");
+	   break;
 		 }
- 		default:{
- 			printf("\n\nN˙mero inv·lido...tente novamente\n");
- 			break;
- 		}
+ 	default:{
+ 	   printf("\n\nN√∫mero inv√°lido...tente novamente\n");
+ 	   break;
  	}
  }
- 	while (op!=5);
- 	system("Pause");
- } 
-
-
+}
+ while (op!=5);
+ system("Pause");
+} 
