6f1edd837f84c298b1e546e17af246038eb9deb9
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-13 00:57:06 -0300

    Função 17

---
+++ Functions/Função_17.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <iostream>
+
+using namespace std;
+
+float divide(float x) {
+	float num, divide1;
+	num = x;
+	divide1 = num / 2;
+	return divide1;
+}
+
+int main() {
+	float numero;
+	setlocale(LC_ALL,"");
+	cout << "Digite um número: ";
+	cin >> numero;
+	cout << "\nO número" << ' ' << numero << " dividido por 2 é: " << divide(numero) << endl << endl;
+	system("pause");
+}
