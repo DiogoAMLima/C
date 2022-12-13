90bd1c7833cd466ec9df855fb32913c1e400f88e
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-12 23:06:04 -0300

    Vetor 7

--- Arrays_Vectors/Vetor 7.cpp
+++ Arrays_Vectors/Vetor 7.cpp
@@ -6,23 +6,23 @@
 using namespace std;
 
 int main() {
-	int x[6], i=0;
-	setlocale(LC_ALL,"");
-	cout << "Digite o valor do vetor na posição x[0]: ";
-	cin >> x[0];
-	cout << "Digite o valor do vetor na posição x[1]: ";
-	cin >> x[1];
-	cout << "Digite o valor do vetor na posição x[2]: ";
-	cin >> x[2];
-	cout << "Digite o valor do vetor na posição x[3]: ";
-	cin >> x[3];
-	cout << "Digite o valor do vetor na posição x[4]: ";
-	cin >> x[4];
-	cout << "Digite o valor do vetor na posição x[5]: ";
-	cin >> x[5];
-	for(i=0;i<6;i++){
-		system("color 4");
-		cout << "\nO valor da posição " << i << ' ' << "multiplicado por 2 é: " << x[i] * 2 << endl; 
-	}
-	system("pause");
+    int x[6], i=0;
+    setlocale(LC_ALL,"");
+    cout << "Digite o valor do vetor na posiÃ§Ã£o x[0]: ";
+    cin >> x[0];
+    cout << "Digite o valor do vetor na posiÃ§Ã£o x[1]: ";
+    cin >> x[1];
+    cout << "Digite o valor do vetor na posiÃ§Ã£o x[2]: ";
+    cin >> x[2];
+    cout << "Digite o valor do vetor na posiÃ§Ã£o x[3]: ";
+    cin >> x[3];
+    cout << "Digite o valor do vetor na posiÃ§Ã£o x[4]: ";
+    cin >> x[4];
+    cout << "Digite o valor do vetor na posiÃ§Ã£o x[5]: ";
+    cin >> x[5];
+    for(i=0;i<6;i++){
+	system("color 4");
+	cout << "\nO valor da posiÃ§Ã£o " << i << ' ' << "multiplicado por 2 Ã©: " << x[i] * 2 << endl; 
+    }
+    system("pause");
 }
