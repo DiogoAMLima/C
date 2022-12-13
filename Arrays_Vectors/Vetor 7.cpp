e0aa46a41e7b2a3d9cfa0e610db89373f87b2cd4
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-12 23:05:31 -0300

    Vetor 7

---
+++ Arrays_Vectors/Vetor 7.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+	int x[6], i=0;
+	setlocale(LC_ALL,"");
+	cout << "Digite o valor do vetor na posição x[0]: ";
+	cin >> x[0];
+	cout << "Digite o valor do vetor na posição x[1]: ";
+	cin >> x[1];
+	cout << "Digite o valor do vetor na posição x[2]: ";
+	cin >> x[2];
+	cout << "Digite o valor do vetor na posição x[3]: ";
+	cin >> x[3];
+	cout << "Digite o valor do vetor na posição x[4]: ";
+	cin >> x[4];
+	cout << "Digite o valor do vetor na posição x[5]: ";
+	cin >> x[5];
+	for(i=0;i<6;i++){
+		system("color 4");
+		cout << "\nO valor da posição " << i << ' ' << "multiplicado por 2 é: " << x[i] * 2 << endl; 
+	}
+	system("pause");
+}
