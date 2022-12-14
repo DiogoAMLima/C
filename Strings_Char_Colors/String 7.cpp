c0f9d374714186bc456fce0e2b16bdcd56700d28
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-14 01:01:16 -0300

    String 7

---
+++ Strings_Char_Colors/String 7.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char string[30], string2[30];
+	int i;
+	cout << "Informe um nome para a primeira String: ";
+	cin >> string;
+	for (i=0; string[i] != '\0'; i++)
+	  string2[i]=string[i];
+	string2[i]='\0';
+	cout << endl;
+	cout << "String 2" << ' ' << string2 << " copiada da String 1" << ' ' << string;
+	return 0;
+} 
