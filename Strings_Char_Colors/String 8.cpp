f5f2d55532fc179adb695a8db2787a9134104baa
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-14 21:45:09 -0300

    String 8

---
+++ Strings_Char_Colors/String 8.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main () {
+	char string[30]="Naruto", string2[30]=" Shippuden";
+	strcat(string, string2);
+	cout << "Strings Concatenadas: " << string << endl << endl;
+	system("pause");
+	return 0;
+}
