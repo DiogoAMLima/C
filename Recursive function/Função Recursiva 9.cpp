550546edf13a0a33668ae61556270fe8a884ccfb
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-17 00:05:13 -0300

    Função Recursiva 9

--- Recursive function/Função Recursiva 9.cpp
+++ Recursive function/Função Recursiva 9.cpp
@@ -8,17 +8,17 @@ using namespace std;
 
 int fat(int N)
 {
-	if (N < 2)
-	return 1;
-	else
+    if (N < 2)
+    return 1;
+    else
     return N*fat(N-1);
 }
 
 int main()
 {
-	int N;
-	setlocale(LC_ALL,"");
-	cout << "Digite um num: ";
-	cin >> N;
-	cout << "\nO resultado é: " << fat(N);
+    int N;
+    setlocale(LC_ALL,"");
+    cout << "Digite um num: ";
+    cin >> N;
+    cout << "\nO resultado Ã©: " << fat(N);
 }
